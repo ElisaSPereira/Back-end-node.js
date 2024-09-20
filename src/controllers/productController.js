023ae9b5a5711cd77453befa88765fcb1b5b26e5
ElisaSPereira <pereiraelisa365@gmail.com>
2024-09-20 08:59:50 -0300

    atualizado

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import { getAllProducts } from '../services/productService.js';
+import { createProduct, getAllProducts, getProductsById } from '../services/productService.js';
 
 export const getProducts = async(req,res) => {
     try {
@@ -9,5 +9,29 @@ export const getProducts = async(req,res) => {
         res.status(500).json({
             error:"Erro ao busca produtos",
         });
+    }
+};
+
+export default createProductController = async (req,res) => {
+    try {
+        const newProduct = await createProduct(req.body);
+        res.status(201).json(newProduct)
+    }
+    catch (error) {
+        res.status(500).json({error: "Erro ao criar produto"});
+    }
+};
+
+export const getProduct = async(req, res)=>{
+    try {
+        const product = await getProductsById(req.params.id)
+        if(product) {
+            res.json(product);
+        }else {
+            res.status(500).json({error: "Produto não encontrado"})
+        }
+    }
+    catch (error) {
+        res.status(500).json({ error: "Erro ao buscar o produto"})
     }
 };
